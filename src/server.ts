5ee145a0d55217ef3764ec3856464b7fdbeb6f4e
Your Full Name <harshal@mamotechnolabs.com>
2026-07-24 22:29:22 +0530

    add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
+import { initDb } from "./config/db";
 import logger from "./config/logger";
 import config from "config";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT: number = config.get("server.port");
     try {
+        await initDb();
+        logger.info("Database connected successfully.");
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -16,4 +19,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
